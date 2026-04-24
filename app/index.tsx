d1bc11277abf9a1aab7c14dbb18bcbfe6bef9330
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2026-04-24 01:28:27 -0400

    setup stylesheet

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={myStyles.container}>
+      <Text style={myStyles.text}>IoT Final Project</Text>
     </View>
+    
   );
 }
+
+const myStyles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems:'center',
+  },
+  text:{
+    color: 'blue',
+    fontSize: 30,
+  }
+});
